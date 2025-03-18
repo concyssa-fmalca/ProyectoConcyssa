a18043004ef501b464e75dc0c65bc2ae5b709c76
fmalca <67565141+concyssa-fmalca@users.noreply.github.com>
2025-03-18 12:28:20 -0500

    Se agregó requisitos de complejidad a la contraseña de usuario

--- ConcyssaWeb/wwwroot/Scripts/Usuario/Usuario.js
+++ ConcyssaWeb/wwwroot/Scripts/Usuario/Usuario.js
@@ -2,6 +2,64 @@
 let contartr = 0;
 
 window.onload = function () {
+
+    // Validación de la contraseña en tiempo real
+    $("#txtContraseña").on("keyup", function () {
+        var password = $(this).val();
+
+        // Mostrar el contenedor de fortaleza solo si hay texto
+        if (password.length > 0) {
+            $("#passwordStrengthContainer").show();
+        } else {
+            $("#passwordStrengthContainer").hide();
+            return;
+        }
+
+        // Verificar cada requisito
+        var lengthValid = password.length >= 8;
+        var lowercaseValid = /[a-z]/.test(password);
+        var uppercaseValid = /[A-Z]/.test(password);
+        var numberValid = /[0-9]/.test(password);
+        var specialValid = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+
+        // Actualizar indicadores visuales para cada requisito
+        updateRequirement("req-length", lengthValid);
+        updateRequirement("req-lowercase", lowercaseValid);
+        updateRequirement("req-uppercase", uppercaseValid);
+        updateRequirement("req-number", numberValid);
+        updateRequirement("req-special", specialValid);
+
+        // Calcular la fortaleza general (de 0 a 100)
+        var strength = 0;
+        if (lengthValid) strength += 20;
+        if (lowercaseValid) strength += 20;
+        if (uppercaseValid) strength += 20;
+        if (numberValid) strength += 20;
+        if (specialValid) strength += 20;
+
+        // Actualizar la barra de progreso
+        $("#passwordStrength")
+            .css("width", strength + "%")
+            .attr("aria-valuenow", strength);
+
+        // Cambiar el color de la barra según la fortaleza
+        if (strength < 40) {
+            $("#passwordStrength").removeClass("bg-warning bg-success").addClass("bg-danger");
+        } else if (strength < 80) {
+            $("#passwordStrength").removeClass("bg-danger bg-success").addClass("bg-warning");
+        } else {
+            $("#passwordStrength").removeClass("bg-danger bg-warning").addClass("bg-success");
+        }
+
+        // Agregar mensaje de error al campo si no cumple todos los requisitos
+        if (strength < 100) {
+            $("[data-valmsg-for='Password']").text("La contraseña no cumple con todos los requisitos").show();
+        } else {
+            $("[data-valmsg-for='Password']").text("").hide();
+        }
+    });
+
+
     var url = "ObtenerUsuarios";
     ConsultaServidor(url);
     CargarEstadosGiro();
@@ -231,6 +289,20 @@ function GuardarUsuario() {
         Swal.fire("Error", "El Campo Sociedad es Obligatorio", "info")
         return
     }
+
+    var isNewUser = $("#txtId").val() === "0";
+
+    // Solo validar la contraseña si es un nuevo usuario o si se ha ingresado una nueva contraseña
+    if (isNewUser || varContraseña.length > 0) {
+        // Verificar todos los requisitos
+        var isValid = validatePassword(varContraseña);
+
+        if (!isValid) {
+            Swal.fire("La contraseña no cumple con todos los requisitos de seguridad. Por favor revise los requisitos indicados.");
+            return
+        }
+    }
+
 
 
     $.post('UpdateInsertUsuario', {
@@ -674,4 +746,32 @@ function ActualizarClave() {
         }
 
     });
+}
+function validatePassword(password) {
+    if (password.length < 8) return false;
+    if (!/[a-z]/.test(password)) return false;
+    if (!/[A-Z]/.test(password)) return false;
+    if (!/[0-9]/.test(password)) return false;
+    if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) return false;
+
+    return true;
+}
+
+// Función para actualizar cada requisito en la lista
+function updateRequirement(id, isValid) {
+    if (isValid) {
+        $("#" + id)
+            .removeClass("text-danger")
+            .addClass("text-success")
+            .find("i")
+            .removeClass("fa-times-circle")
+            .addClass("fa-check-circle");
+    } else {
+        $("#" + id)
+            .removeClass("text-success")
+            .addClass("text-danger")
+            .find("i")
+            .removeClass("fa-check-circle")
+            .addClass("fa-times-circle");
+    }
 }
